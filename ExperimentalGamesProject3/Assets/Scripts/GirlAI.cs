3246d782b3df1bc57b911e67db20995513763a80
lazeuli <164913287+lazeuli@users.noreply.github.com>
2025-05-12 21:34:26 -0400

    adding girl anims

--- ExperimentalGamesProject3/Assets/Scripts/GirlAI.cs
+++ ExperimentalGamesProject3/Assets/Scripts/GirlAI.cs
@@ -9,22 +9,39 @@ public class GirlAI : MonoBehaviour
     public float reachDist;
     public int currentPoint = 0;
     private bool move = false;
-    // Start is called before the first frame update
 
-    // Update is called once per frame
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
+
     void Update()
     {
         if (move)
         {
             Vector3 directionVector = path[currentPoint].position - transform.position;
             directionVector.y = 0;
+
             if (directionVector.magnitude <= reachDist && currentPoint < path.Length - 1)
             {
                 currentPoint++;
                 move = false;
+                animator.SetBool("isWalking", false); 
+                return;
             }
+
+
             directionVector = directionVector.normalized;
             transform.position += directionVector * Time.deltaTime * speed;
+
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
         }
     }
 
